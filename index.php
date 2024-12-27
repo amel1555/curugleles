dc8ed19a223993d79a75ce3f6c5f061b55b33654
amel1555 <amelianurul706@gmail.com>
2024-12-27 15:45:35 +0700

    perbaruan

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=pool" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=check_box_outline_blank" />
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
     <link rel="stylesheet" href="assets/css/style.css" />
   </head>
   <body>
@@ -38,121 +40,121 @@
     </header>
 
     <!--Section Service-->
-<section id="services" class="services">
+    <section id="services" class="services">
     <h2>Service Kami</h2>
     <div class="service-container">
-      <div class="service-box">
-        <div class="icon">🎟</div>
-        <h3>Tiket</h3>
-        <p>Kamu dapat memesan tiket di sini</p>
+        <div class="service-box">
+            <div class="icon">🎟</div>
+            <h3>Tiket</h3>
+            <p>Kamu dapat memesan tiket di sini</p>
+        </div>
+        <div class="service-box">
+            <div class="icon">🏕</div>
+            <h3>Camping</h3>
+            <p>Kamu dapat memesan perlengkapan berkemah di sini</p>
+        </div>
+        <div class="service-box">
+            <div class="icon">✈</div>
+            <h3>Tour Plan</h3>
+            <p>Kami menyediakan perjalanan tour ke curug</p>
+        </div>
       </div>
-      <div class="service-box">
-        <div class="icon">🏕</div>
-        <h3>Camping</h3>
-        <p>Kamu dapat memesan perlengkapan berkemah di sini</p>
-      </div>
-      <div class="service-box">
-        <div class="icon">✈</div>
-        <h3>Tour Plan</h3>
-        <p>Kami menyediakan perjalanan tour ke curug</p>
-      </div>
-    </div>
-  </section>
+    </section>
 
-  <!--Section Destinasi-->
-    <!-- Destinasi Populer -->
-  <section class="section" id="destinasi">
-    <h2>Destinasi Populer</h2>
-    <div class="popular-destinations container">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.7355931599504!2d108.38671506983016!3d-6.8019866514611325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f219a16c7466b%3A0x2bb5e9ad40060c49!2sBumi%20Perkemahan%20Curug%20Leles!5e0!3m2!1sid!2sid!4v1735104793025!5m2!1sid!2sid" width="520" height="520" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-      <div class="destinations">
-        <div class="destination-card">
-          <img src="assets/images/image1.jpeg" alt="Kolam Pemandian" />
-          <div>
-            <h3>Curug Leles</h3>
-            <p class="text-wrap">Rasakan keindahan eksotis Curug setinggi 7 meter, dengan air terjun segar dan pemandangan alam hijau nan asri. Cocok untuk melepas penat dan menyatu dengan alam. Kunjungi dan temukan pesonanya!</p>
+      
+    <!--Section Destinasi-->
+      <!-- Destinasi Populer -->
+    <section class="section" id="destinasi">
+      <h2>Destinasi Populer</h2>
+      <div class="popular-destinations container">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.7355931599504!2d108.38671506983016!3d-6.8019866514611325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f219a16c7466b%3A0x2bb5e9ad40060c49!2sBumi%20Perkemahan%20Curug%20Leles!5e0!3m2!1sid!2sid!4v1735104793025!5m2!1sid!2sid" width="520" height="520" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div class="destinations">
+          <div class="destination-card">
+            <img src="assets/images/image1.jpeg" alt="Kolam Pemandian" />
+            <div>
+              <h3>Curug Leles</h3>
+              <p class="text-wrap">Rasakan keindahan eksotis Curug setinggi 7 meter, dengan air terjun segar dan pemandangan alam hijau nan asri. Cocok untuk melepas penat dan menyatu dengan alam. Kunjungi dan temukan pesonanya!</p>
+            </div>
           </div>
-        </div>
-        <div class="destination-card">
-          <img src="assets/images/destinasi 2.jpg" alt="Kolam Pemandian" />
-          <div>
-            <h3>Bumi Perkemahan</h3>
-            <p class="text-wrap">Nikmati pengalaman camping seru di bumi perkemahan luas, dikelilingi hutan pinus yang menyejukkan. Cocok untuk petualangan dan bersantai di tengah alam.</p>
+          <div class="destination-card">
+            <img src="assets/images/destinasi 2.jpg" alt="Kolam Pemandian" />
+            <div>
+              <h3>Bumi Perkemahan</h3>
+              <p class="text-wrap">Nikmati pengalaman camping seru di bumi perkemahan luas, dikelilingi hutan pinus yang menyejukkan. Cocok untuk petualangan dan bersantai di tengah alam.</p>
+            </div>
           </div>
-        </div>
-        <div class="destination-card">
-          <img src="assets/images/destinasi 3.jpg" alt="Kolam Pemandian" />
-          <div>
-            <h3>Kolam Pemandian</h3>
-            <p class="text-wrap">Tepat di bawah air terjun, terdapat kolam alami yang menyegarkan dan aman untuk semua usia. Sempurna untuk relaksasi keluarga sambil menikmati keindahan alam.</p>
+          <div class="destination-card">
+            <img src="assets/images/destinasi 3.jpg" alt="Kolam Pemandian" />
+            <div>
+              <h3>Kolam Pemandian</h3>
+              <p class="text-wrap">Tepat di bawah air terjun, terdapat kolam alami yang menyegarkan dan aman untuk semua usia. Sempurna untuk relaksasi keluarga sambil menikmati keindahan alam.</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
 
-  <!--Section Gallery-->
-    <div class="gallery-section">
-    <h2>Galeri Pengunjung</h2>
-    <div class="gallery">
-      <img src="assets/images/galeri pengunjung 1.jpg" alt="Foto Pengunjung 1" />
-      <img src="assets/images/galeri pengunjung 2.jpg" alt="Foto Pengunjung 2" />
-      <img src="assets/images/galeri pengunjung 3.jpg" alt="Foto Pengunjung 3" />
-      <img src="assets/images/galeri pengunjung 4.jpg" alt="Foto Pengunjung 4" />
-      <img src="assets/images/galeri pengunjung 5.jpg" alt="Foto Pengunjung 5" />
-      <img src="assets/images/galeri pengunjung 5.jpg" alt="Foto Pengunjung 6" />
-    </div>
+    <!--Section Gallery-->
+      <div class="gallery-section">
+        <h2>Galeri Pengunjung</h2>
+        <div class="gallery">
+        <img src="assets/images/galeri pengunjung 1.jpg" alt="Foto Pengunjung 1" />
+        <img src="assets/images/galeri pengunjung 2.jpg" alt="Foto Pengunjung 2" />
+        <img src="assets/images/galeri pengunjung 3.jpg" alt="Foto Pengunjung 3" />
+        <img src="assets/images/galeri pengunjung 4.jpg" alt="Foto Pengunjung 4" />
+        <img src="assets/images/galeri pengunjung 5.jpg" alt="Foto Pengunjung 5" />
+        <img src="assets/images/galeri pengunjung 5.jpg" alt="Foto Pengunjung 6" />
+      </div>
     
-    <button class="more-button">Lainnya</button>
+      <button class="Lainnya">Lainnya</button>
     </div>
 
+    <!-- pesan mudah-->
+      <h1 class="custom-title">Pesan Liburanmu dengan Mudah dan Cepat</h1>
+        <div class="content">
+          <!-- Nomor 1 -->
+          <h2>1. Pilih Destinasi Wisata</h2>
+            <div class="custom-list">
+              <p>a. Bumi Perkemahan Curug Leles</p>
+              <p>b. Curug Leles</p>
+              <p>c. Kolam Pemandian</p>
+            </div>
 
-  <!-- pesan mudah-->
-  <h1 class="custom-title">Pesan Liburanmu dengan Mudah dan Cepat</h1>
-  <div class="content">
-      <!-- Nomor 1 -->
-      <h2>1. Pilih Destinasi Wisata</h2>
-      <div class="custom-list">
-          <p>a. Bumi Perkemahan Curug Leles</p>
-          <p>b. Curug Leles</p>
-          <p>c. Kolam Pemandian</p>
+          <!-- Nomor 2 -->
+          <h3>2. Lengkapi Data Diri dan Informasi Pesanan</h3>
+            <div class="custom-list">
+            <p>a. Isi nama lengkap</p>
+            <p>b. Masukkan nomor telepon</p>
+            <p>c. Tambahkan email untuk konfirmasi</p>
+          </div>
+
+          <!-- Nomor 3 -->
+          <h4>3. Selesaikan Pembayaran</h4>
+            <div class="custom-list">
+            <p>a. Pilih metode pembayaran</p>
+            <p>b. Lakukan pembayaran sesuai instruksi</p>
+            <p>c. Konfirmasi pembayaran untuk mendapatkan tiket</p>
+          </div>
       </div>
 
-      <!-- Nomor 2 -->
-      <h3>2. Lengkapi Data Diri dan Informasi Pesanan</h3>
-      <div class="custom-list">
-          <p>a. Isi nama lengkap</p>
-          <p>b. Masukkan nomor telepon</p>
-          <p>c. Tambahkan email untuk konfirmasi</p>
+      <!--Section Review-->
+      <div class="review-section">
+        <div class="review-box">
+          <div class="profile-ulasan"></div>
+          <h3 class="review-name">Amelia Nurul Islamiyah</h3>
+          <p class="review-text">
+            "Pengalaman yang luar biasa! Tempat ini menawarkan suasana alam yang menenangkan serta kolam alami yang aman untuk semua usia. Cocok untuk relaksasi keluarga atau sekadar menikmati keindahan alam. Sangat direkomendasikan!"
+          </p>
+        </div>
       </div>
 
-      <!-- Nomor 3 -->
-      <h4>3. Selesaikan Pembayaran</h4>
-      <div class="custom-list">
-          <p>a. Pilih metode pembayaran</p>
-          <p>b. Lakukan pembayaran sesuai instruksi</p>
-          <p>c. Konfirmasi pembayaran untuk mendapatkan tiket</p>
-      </div>
-  </div>
 
-  <!--Section Review-->
-  <div class="review-section">
-    <div class="review-box">
-      <div class="profile-ulasan"></div>
-      <h3 class="review-name">Amelia Nurul Islamiyah</h3>
-      <p class="review-text">
-        "Pengalaman yang luar biasa! Tempat ini menawarkan suasana alam yang menenangkan serta kolam alami yang aman untuk semua usia. Cocok untuk relaksasi keluarga atau sekadar menikmati keindahan alam. Sangat direkomendasikan!"
-      </p>
-    </div>
-  </div>
-
-
-  <!- Footer -->
-  <footer>
-    <div class="footer-container">
-      <div class="footer-section logo-section">
-        <h2>Nyurug Kuy</h2>
-        <p>Temukan Keindahan Alam di Curug Leles</p>
+      <!--Footer -->
+    <footer>
+      <div class="footer-container">
+        <div class="footer-section logo-section">
+          <h2>Nyurug Kuy</h2>
+          <p>Temukan Keindahan Alam di Curug Leles</p>
       </div>
       <div class="footer-section">
         <h3>Contact</h3>
@@ -194,3 +196,5 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
+
+
